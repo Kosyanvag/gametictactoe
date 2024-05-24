7cec4fbb9266c913b2712af2a930a671a864eb6d
Vagarshak Kosyan <vag.kosyan@gmail.com>
2024-05-24 15:23:12 +0400

    Version 1.0

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Game from "./components/Game";
+import "./index.css";
+
+const App = () => {
+  return (
+    <div className="app">
+      <Game />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Square.jsx
@@ -0,0 +1,9 @@
+const Square = ({ value, onClick }) => {
+  return (
+    <div className="square" onClick={onClick}>
+      {value}
+    </div>
+  );
+};
+
+export default Square;
